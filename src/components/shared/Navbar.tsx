cfdc2ead85acfd2c9c76fbb9232ebd653f940d9a
jewelmia24 <jewelmiyacst@gmail.com>
2025-05-10 15:23:22 +0600

    make navbar responsive

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -4,8 +4,10 @@ import { Button } from "../ui/button";
 import Link from "next/link";
 import { Gwendolyn } from "next/font/google";
 import { useTheme } from "next-themes";
-import { Menu, Moon, Sun } from "lucide-react";
+import { Menu, Moon, Sun, X} from "lucide-react";
 import { useMounted } from "@/hooks/useMounted";
+import { useState } from "react";
+import { Separator } from "../ui/separator";
 const gwendolyn = Gwendolyn({
   subsets: ["latin"],
   weight: ["400", "700"], // Gwendolyn supports 400 & 700
@@ -14,6 +16,7 @@ const menu = ["Home", "Project", "Blogs", "Contact"];
 
 const Navbar = () => {
   const { setTheme, resolvedTheme } = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
   const mounted = useMounted();
   if (!mounted) return null;
   const handleToggleTheme = () => {
@@ -52,7 +55,7 @@ const Navbar = () => {
         </div>
         {/* mobile & tablet view */}
         <div className=" flex justify-between items-center lg:hidden">
-          <Menu />
+          <Menu onClick={() => setIsOpen(!isOpen)} />
           <div className=" flex items-center gap-1.5">
             <Link href={"/"}>
               <h1
@@ -68,6 +71,39 @@ const Navbar = () => {
                 <Sun size={32} />
               )}
             </Button>
+          </div>
+        </div>
+        {/* blur bg */}
+        {isOpen && (
+          <div
+            className="fixed inset-0 z-40 bg-black/20 backdrop-blur-xs transition-opacity opacity-50 duration-300"
+            onClick={()=>setIsOpen(false)}
+          />
+        )}
+        {/* mobile & tablet menu */}
+        <div
+          className={`lg:hidden fixed top-0 left-0 w-[50%] bg-background shadow-xl z-50 transition-transform duration-300 ease-in-out ${
+            isOpen
+              ? "translate-x-0 mt-9 ml-4 rounded-md"
+              : "-translate-x-full "
+          }`}
+        >
+          <div className="flex flex-col gap-2 p-6">
+            {menu.map((item, index) => (
+              <Link
+                key={index}
+                href={`#${item}`}
+                onClick={() => setIsOpen(false)}
+                className="text-lg hover:underline"
+              >
+                {item}
+              </Link>
+            ))}
+            <Separator className="mb-2"/>
+            <Button variant="outline" onClick={() => setIsOpen(false)}>
+              Download Resume
+            </Button>
+            <X className="absolute right-3 top-3" onClick={()=>setIsOpen(!isOpen)}/>
           </div>
         </div>
       </div>
